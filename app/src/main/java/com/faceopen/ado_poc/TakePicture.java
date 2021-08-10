6e1d2c51f5e162b7c677bb29368e5d3b01e3ee7e
rana-gaurav <71083851+rana-gaurav@users.noreply.github.com>
2021-08-10 18:01:06 +0530

    Camera permission added to avoid crash

--- app/src/main/java/com/faceopen/ado_poc/TakePicture.java
+++ app/src/main/java/com/faceopen/ado_poc/TakePicture.java
@@ -1,16 +1,23 @@
 package com.faceopen.ado_poc;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.hardware.Camera;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -30,14 +37,11 @@ import java.io.IOException;
 import static android.content.ContentValues.TAG;
 
 public class TakePicture extends AppCompatActivity {
-Button b1, b2 ;
-ImageView iv_preview ;
-
+    private Button b1, b2 ;
+    private ImageView iv_preview ;
     private static final int REQUEST_CAMERA_PERMISSION = 1;
-
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private Bitmap bitmap;
-
     private Camera mCamera;
     private CameraPreview mPreview;
 
@@ -45,22 +49,13 @@ ImageView iv_preview ;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
-
         b1 = (Button)findViewById(R.id.button_capture) ;
-
         iv_preview = (ImageView)findViewById(R.id.iv_preview) ;
-
-        // Create an instance of Camera
-        mCamera = getCameraInstance(this);
-        mCamera.setDisplayOrientation(90);
-
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
-
-
-
+        if (checkCameraPermission()) {
+            startPreView();
+        } else {
+            requestPermission();
+        }
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,18 +81,9 @@ ImageView iv_preview ;
                         }
                     }
                 });
-
                 iv_preview.setImageBitmap(bitmap);
-
-
             }
         });
-
-
-
-
-
-
     }
 
     public static Camera getCameraInstance(TakePicture context){
@@ -111,7 +97,71 @@ ImageView iv_preview ;
         return c; // returns null if camera is unavailable
     }
 
-   }
+    private boolean checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
+        }
+        return true;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 100:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+                    startPreView();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow access permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(TakePicture.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private void startPreView(){
+        mCamera = getCameraInstance(this);
+        mCamera.setDisplayOrientation(90);
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+}
 
 
 
